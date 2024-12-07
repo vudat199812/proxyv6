9f05625babebe1108bbc4595afccd2b712c11a1a
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-07 08:49:22 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp scripts/init.d/proxy.sh /etc/init.d/3proxy
+    cp ./scripts/init.d/proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
