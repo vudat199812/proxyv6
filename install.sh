3e2ec7fd02cb352f135c9fa94d24e5a750294ee6
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-08 10:28:46 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -116,18 +116,16 @@ LAST_PORT=$(($FIRST_PORT + $COUNT))
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh etc/rc.d
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.d/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-touch /etc/rc.d/rc.local
+
 cat >>/etc/rc.d/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 systemctl start 3proxy
 EOF
-chmod +x /etc/rc.d/rc.local
 bash /etc/rc.d/rc.local
 
 gen_proxy_file_for_user
