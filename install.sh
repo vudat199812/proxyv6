f2988396c93f79ef6f59f4b8ca808f47144855ef
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-08 02:58:31 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,9 +31,7 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
-    cp ./scripts/3proxy.service /etc/systemd/system/3proxy.service
-    systemctl daemon-reload
-    
+    cp ./scripts/rc.d/3proxy.sh /etc/init.d/3proxy
     cd $WORKDIR
 }
 
@@ -94,7 +92,7 @@ yum -y install gcc net-tools bsdtar zip >/dev/null
 chmod +x /etc/rc.d/rc.local
 systemctl enable rc-local
 systemctl start rc-local
-bash /etc/rc.d/rc.local
+bash /etc/rc.local
 check_iptables_install
 install_3proxy
 
@@ -117,7 +115,7 @@ LAST_PORT=$(($FIRST_PORT + $COUNT))
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
@@ -127,7 +125,6 @@ bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
-chmod +x ${WORKDIR}/boot_*.sh
+
 
 gen_proxy_file_for_user
-systemctl start 3proxy
