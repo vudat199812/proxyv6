ff8cfc870e0151876ae2c00d3edb6f8ca9053fb0
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-08 15:46:14 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -137,7 +137,6 @@ chmod +x ${WORKDIR}/boot_*.sh /etc/rc.d/rc.local
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.d/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
