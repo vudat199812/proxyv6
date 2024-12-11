adc20af391a342a087fc94d7a417dfcfbc01a22d
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-11 14:12:00 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,6 +32,7 @@ install_3proxy() {
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
+    cp bin/3proxy.service /etc/systemd/system/
     cd $WORKDIR
 }
 
@@ -116,7 +117,7 @@ cat >>/etc/rc.d/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
+systemctl start 3proxy
 EOF
 
 bash /etc/rc.d/rc.local
