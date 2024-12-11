8c41df2cc6a337f2c41644b37151c48fdb7ecf27
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-11 10:37:15 -0800

    rotate_ipv6.sh

--- rotate_ipv6.sh
+++ rotate_ipv6.sh
@@ -2,7 +2,7 @@
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 clear_proxy_and_file(){
-	if ip -6 addr flush dev eth0; then
+	if ip -6 addr show dev eth0 | grep "inet6" | grep -v "::1/64" | grep -v "fe80::" | awk '{print $2}' | xargs -I {} sudo ip -6 addr del {} dev eth0; then
 	    echo "Đã xóa các địa chỉ IPv6 không mong muốn thành công."
 	else
 	    echo "Lỗi khi xóa địa chỉ IPv6, tiếp tục chạy lệnh tiếp theo."
