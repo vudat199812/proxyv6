757deb457e2d9f07cf22b31c8fb49eddc675bc94
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-11 11:20:19 -0800

    Update rotate_ipv6.sh

--- rotate_ipv6.sh
+++ rotate_ipv6.sh
@@ -4,6 +4,7 @@ WORKDATA="${WORKDIR}/data.txt"
 clear_proxy_and_file(){
 	echo "clear_proxy_and_file"
 	if ip -6 addr show dev eth0 | grep "inet6" | grep -v "::1/64" | awk '{print $2}' | xargs -I {} sudo ip -6 addr del {} dev eth0; then
+	    sudo ifdown eth0 && sudo ifup eth0
 	    echo "Đã xóa các địa chỉ IPv6 không mong muốn thành công."
 	else
 	    echo "Lỗi khi xóa địa chỉ IPv6, tiếp tục chạy lệnh tiếp theo."
