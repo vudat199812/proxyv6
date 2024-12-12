7e3a2cf5c17a3381a62f8147ffad85ad90fb738b
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-12 05:29:39 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,9 +30,7 @@ install_3proxy() {
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.4
     make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp bin/3proxy /usr/local/etc/3proxy/bin/3proxy
-    cp ./scripts/3proxy.service /etc/systemd/system/3proxy.service
+    cp root/3proxy-0.9.4/scripts/3proxy.service /etc/systemd/system/3proxy.service
     systemctl daemon-reload
     cd $WORKDIR
 }
@@ -108,7 +106,8 @@ gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.d/rc.local
-gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+>/root/3proxy-0.9.4/scripts/3proxy.cfg
+gen_3proxy >/root/3proxy-0.9.4/scripts/3proxy.cfg
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 systemctl start 3proxy
