2646c0254965686e7876f0101e24cdfac655123d
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-11 18:21:58 -0800

    Update rotate_ipv6.sh

--- rotate_ipv6.sh
+++ rotate_ipv6.sh
@@ -104,14 +104,11 @@ chmod +x ${WORKDIR}/boot_*.sh /etc/rc.d/rc.local
 echo "gen_3proxy"
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-cat >>/etc/rc.d/rc.local <<EOF
+
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-EOF
 
-bash /etc/rc.d/rc.local
+
 echo "gen_proxy_file_for_user"
 gen_proxy_file_for_user
 upload_proxy
