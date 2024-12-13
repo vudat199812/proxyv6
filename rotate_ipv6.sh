631aa857b02f22a97a907ee61757875d9587fd3a
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-12 23:32:57 -0800

    Update rotate_ipv6.sh

--- rotate_ipv6.sh
+++ rotate_ipv6.sh
@@ -103,7 +103,7 @@ gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
-/usr/local/etc/3proxy/3proxy.cfg
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 echo "gen_proxy_file_for_user"
 gen_proxy_file_for_user
 systemctl restart NetworkManager
