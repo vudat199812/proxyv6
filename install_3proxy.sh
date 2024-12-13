38c14520392bcd330b3ad6d1e270dc55d13a1a8e
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-12 18:56:08 -0800

    Update install_3proxy.sh

--- install_3proxy.sh
+++ install_3proxy.sh
@@ -13,7 +13,7 @@ install_3proxy() {
     URL="https://raw.githubusercontent.com/vudat199812/proxyv6/main/3proxy-3proxy-0.9.4.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-0.9.4
-    make -f Makefile.Linux
+    make -f Makefile.unix-install
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp bin/3proxy /usr/local/etc/3proxy/bin/
     cd $WORKDIR
