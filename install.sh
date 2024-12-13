b41eebc40d219a5976f90fde7617e1b45c8a555a
vudat199812 <72659591+vudat199812@users.noreply.github.com>
2024-12-12 22:12:53 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ install_3proxy() {
     cp bin/3proxy /usr/local/etc/3proxy/bin/
     cp scripts/3proxy.service /etc/systemd/system/3proxy.service
     sed -i 's|Environment=CONFIGFILE=/etc/3proxy/3proxy.cfg|Environment=CONFIGFILE=/usr/local/etc/3proxy/3proxy.cfg|' /etc/systemd/system/3proxy.service
-    sed -i 's|ExecStart=/bin/3proxy ${CONFIGFILE}|ExecStart=/etc/3proxy/bin/3proxy ${CONFIGFILE}|' /etc/systemd/system/3proxy.service
+    sed -i 's|ExecStart=/bin/3proxy ${CONFIGFILE}|ExecStart=/usr/local/etc/3proxy/bin/3proxy ${CONFIGFILE}|' /etc/systemd/system/3proxy.service
     sed -i 's/RestartSec=60s/RestartSec=0s/' /etc/systemd/system/3proxy.service
     chmod +x /usr/local/etc/3proxy/bin/3proxy
     cd $WORKDIR
